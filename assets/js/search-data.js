68e1ce32d153fb654234897bad725b220773ab5b
coryair <123841764+coryair@users.noreply.github.com>
2025-01-01 19:38:54 +0000

    Deploying to gh-pages from @ coryair/coryair.github.io@2645a2948ce48890ac88ce392ab08ae1def6f9f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -20,7 +20,7 @@ ninja.data = [{
       
         title: "Capturing AWS Organization Events with EventBridge and Buses and Taking Action",
       
-      description: "Learn how to reduce support tickets and automate responses by capturing AWS Organization events with EventBridge and event buses. Step-by-step guide for implementing cross-account event tracking and automated remediation using CloudTrail logs.",
+      description: "Learn how to reduce support tickets and automate responses by capturing AWS Organization events with EventBridge and event buses.",
       section: "Posts",
       handler: () => {
         
